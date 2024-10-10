5cc75c3945a8d43d60faed25f90fca02b7b5d105
MaBeing <john9912@naver.com>
2024-10-10 10:30:48 +0900

    Bullet, Enemy Spawn 구현

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
